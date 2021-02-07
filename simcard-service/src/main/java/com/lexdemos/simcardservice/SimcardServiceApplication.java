a848b1f984f97264d2e60c64b9f8cca7cd0315b1
Manoj Kumar <manking.r318@gmail.com>
2021-02-07 21:06:01 +0530

    discovery-server and api-gateway added

--- simcard-service/src/main/java/com/lexdemos/simcardservice/SimcardServiceApplication.java
+++ simcard-service/src/main/java/com/lexdemos/simcardservice/SimcardServiceApplication.java
@@ -2,8 +2,10 @@ package com.lexdemos.simcardservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SimcardServiceApplication {
 
 	public static void main(String[] args) {
